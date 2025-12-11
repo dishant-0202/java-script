89ec1985a9cc3ab99f1562daa34cc828389c9f5d
dishant-0202 <dishantrudani592@gmail.com>
2025-12-11 17:53:00 +0530

    add new file

---
+++ basic-info.js
@@ -0,0 +1,44 @@
+//  function statement   or    function declaration...
+
+function a() {
+    let x = 10;
+    console.log(x);
+}
+a();
+
+
+//  function expression ...
+
+var b = function () {
+    let x = 20; // only one difference between function statement or function declaration and function expression  is hoisting...
+    console.log(x); // call function before creation of function is give error in function expression but not in function statement or function declaration...
+} //this is small difference...
+b();
+
+
+//  aninymous function ...
+
+function () {
+    //  function without name is called aninymous function...    
+}
+//  named function expression ...
+
+var b = function a() {
+    let x = 20;
+    console.log(x); // give name in function expression is called named function expression...
+}
+b();
+
+//  difference between paremeter and arguments ...
+
+//parameter = The variable name used in a function definition.
+function add(a, b) { // a and b are PARAMETERS
+    console.log(a + b);
+}
+//a and b are a parameters...
+
+//argument = The actual value passed to a function when calling it.
+add(x, y);
+
+// x and y are arguments...
+
